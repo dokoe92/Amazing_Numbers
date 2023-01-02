db0175384d8202f843ad75e3150a72bca5ec1696
Dominik Köberl <dokoe92@gmail.com>
2023-01-02 11:39:56 +0100

    first commit

---
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -0,0 +1,66 @@
+package numbers;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        write your code here
+        checkIfBuzz();
+    }
+
+    public static void checkIfBuzz() {
+        int input = 0;
+        try {
+            System.out.println("Enter a natural number: ");
+            input = new Scanner(System.in).nextInt();
+            if (input > 0) {
+            } else {
+                System.out.println("This number is not natural!");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("not a number!");
+        }
+        System.out.println(isEven(input) ? "This number is Even." : "This number is Odd.");
+        System.out.println((endsWithSeven(input) || isDivisibleBySeven(input)) ? "It is a Buzz number." : "It is not a Buzz number");
+        explainBuzz(input);
+
+    }
+
+
+    public static boolean isEven(int input) {
+        String intToString = Integer.toString(input);
+        int lastDigit = intToString.charAt(intToString.length()-1);
+        return lastDigit % 2 == 0;
+    }
+
+    public static boolean endsWithSeven (int input) {
+        String intToString = Integer.toString(input);
+        int lastDigit = Character.getNumericValue(intToString.charAt(intToString.length()-1));
+        return lastDigit ==  7;
+    }
+
+    public static boolean isDivisibleBySeven(int input) {
+        return input % 7 == 0;
+    }
+
+    public static void explainBuzz(int input) {
+        System.out.println("Explanation: ");
+        if (endsWithSeven(input) && isDivisibleBySeven(input)) {
+            System.out.println(input + " is divisible by 7 and ends with 7");
+        } else if (endsWithSeven(input)) {
+            System.out.println(input + " ends with 7");
+        } else if (isDivisibleBySeven(input)) {
+            System.out.println(input + " is divisible by 7");
+        } else {
+            System.out.println(input + " is neither divisible by 7 nor does it end with 7");
+        }
+
+
+
+
+    }
+
+
+
+}
